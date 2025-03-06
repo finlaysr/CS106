33e5132502c5705058543588d60392ab25b12a57
finlaysr <finlayrobb@icloud.com>
2025-03-06 22:06:37 +0000

    Q4a done

--- Test1/Q4a.asm
+++ Test1/Q4a.asm
@@ -1,3 +1,24 @@
 # assume a word is given in $a0 and a bit position in $a1
+.data
+  word: .word 0xFA070010
+
+.text 
+#lw $a0 word
+#li $a1 4
 
 extract:
+  bgt $a1 31 Error
+  blt $a1 0  Error
+  
+  li $t7 1
+  sllv $t7 $t7 $a1
+  and $a0 $a0 $t7
+  
+  j Return 
+  
+Error:
+  break 
+
+Return:
+  
+  j extractReturn
