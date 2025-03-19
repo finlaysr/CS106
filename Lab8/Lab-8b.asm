c3a129037898c0ea4d3f101469a80b45f67037a7
finlaysr <finlayrobb@icloud.com>
2025-03-19 22:00:59 +0000

    tidied up some comments

--- Lab8/Lab-8b.asm
+++ Lab8/Lab-8b.asm
@@ -21,17 +21,17 @@ fib:
   sw $ra ($sp)
   sw $a0 4($sp)
   
-  jal Counter    #add 1 to counter
+  jal Counter       #add 1 to counter
   
-  lw $a0 4($sp)  #counter messes up a0
-  move $v0 $a0   
+  lw $a0 4($sp)     #counter messes up a0 so load it back
+  move $v0 $a0      #v0 = a0
   ble $a0 1 fibEnd  #if done then stop
   
   addi $a0 $a0 -1
   jal fib           #fib(n-1)
-  sw $v0 8($sp)     #save the result to stack
+  sw $v0 8($sp)     #save result to stack
   
-  lw $a0 4($sp)     #get a0 back again
+  lw $a0 4($sp)     #get original a0 back
   addi $a0 $a0 -2   
   jal fib           #fib(n-2)
   
