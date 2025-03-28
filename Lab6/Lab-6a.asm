5d32b7eece269fe8035184e03beb3230ed69a2e2
finlaysr <finlayrobb@icloud.com>
2025-03-28 12:07:36 +0000

    Started Lab 6

--- Lab6/Lab-6a.asm
+++ Lab6/Lab-6a.asm
@@ -18,11 +18,26 @@ CodeToTest:                   # label for running tests
 # Do not use mul or div.
 # ----------------------------------------------------------------
 # YOUR CODE STARTS HERE
+lh $t0 ($a0)   #M
+lh $t1 ($a1)   #Q
 
+li $v0 0       #A
 
+Main:
+  andi $t3 $t1 1 #LSB of Q
+  beqz $t3 SkipIf
 
+  addu $v0 $v0 $t0  #A = A + M
+
+  SkipIf:
+    sll $t0 $t0 1   #shift M left by 1
+    srl $t1 $t1 1   #shift Q right by 1
+  
+  bnez $t1 Main
+  
+  
 # YOUR CODE ENDS HERE
 # ----------------------------------------------------------------
 
 # UNCOMMENT THE FOLLOWING LINE TO TEST YOUR CODE
-#.include "Lab-6a-Tests.asm"
+.include "Lab-6a-Tests.asm"
--- Lab6/Lab-6b.asm
+++ Lab6/Lab-6b.asm
@@ -21,11 +21,48 @@ CodeToTest:                   # label for running tests
 # Do not use mul or div.
 # ----------------------------------------------------------------
 # YOUR CODE STARTS HERE
+li $a0 0 #answer
+li $s1 0 #counter from 0 -> len
+move $s2 $a2
 
 
 
+Loop:
+  
+  lh $t0 ($a0) 
+  lh $t1 ($a1) 
+  li $v0 0
+  
+  jal Mult
+  addu $s0 $s0 $v0 
+  
+  addi $a0 $a0 2    #next vecctor A value
+  addi $a1 $a1 2    #next vecctor B value
+  
+  addi $s1 $s1 1    #counter ++
+  bltu $s1 $s2 Loop
+  
+  move $v0 $s0
+  
+j End
+  
+
+
+Mult:
+  andi $t2 $t1 1
+  beqz $t2 SkipIf
+  addu $v0 $v0 $t0
+  SkipIf:
+    sll $t0 $t0 1
+    srl $t1 $t1 1
+  
+  bnez $t2 Mult
+  jr $ra
+  
+End:
+  
 # YOUR CODE ENDS HERE
 # ----------------------------------------------------------------
 
 # UNCOMMENT THE FOLLOWING LINE TO TEST YOUR CODE
-#.include "Lab-6b-Tests.asm"
+.include "Lab-6b-Tests.asm"
